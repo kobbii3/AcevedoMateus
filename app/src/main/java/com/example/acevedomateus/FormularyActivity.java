c94708bc223c0c1ab6a224051006f1c4b1a6c1ac
Jair Acevedo <jairjoseacevedomateus123@hotmail.com>
2023-04-11 10:08:03 -0500

    FormularyActivity

---
+++ app/src/main/java/com/example/acevedomateus/FormularyActivity.java
@@ -0,0 +1,13 @@
+package com.example.acevedomateus;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormularyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulary);
+    }
+}
