d2a83d5800d14686b577e60052ab6e0cfaf32d1a
Jair Acevedo <jairjoseacevedomateus123@hotmail.com>
2023-04-12 15:55:03 -0500

    Adelanto 12/04/2023

--- app/src/main/java/com/example/acevedomateus/LoginActivity.java
+++ app/src/main/java/com/example/acevedomateus/LoginActivity.java
@@ -2,13 +2,57 @@ package com.example.acevedomateus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText etUsuario, etPassword;
+    private SharedPreferences misPreferencias;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        referenciar();
+
+        misPreferencias = getSharedPreferences("tienda_app", MODE_PRIVATE);
+
+        //VERIFICAR SI ESTÁ LOGUEADO
+        if(misPreferencias.getBoolean("logueado", false)){
+            Intent miIntent = new Intent(this, MainActivity.class);
+            startActivity(miIntent);
+            finish();
+        }
+    }
+
+    private void referenciar(){
+        etUsuario = findViewById(R.id.et_user_login);
+        etPassword = findViewById((R.id.et_user_contraseña));
+    }
+
+    public void clickIniciarSesion(View view){
+        String PASS = "123456";
+        String USER = "Jair";
+
+        String passUser = etPassword.getText().toString();
+        String userUser = etUsuario.getText().toString();
+
+        if(PASS.equals(passUser)&&USER.equals(userUser)){
+
+            SharedPreferences.Editor myEditor = misPreferencias.edit();
+            myEditor.putBoolean("logueado", true);
+            myEditor.apply();
+
+            Intent miIntent = new Intent(this, MainActivity.class);
+            startActivity(miIntent);
+            finish();
+        }else{
+            Toast.makeText(this, "CREDENCIALES INCORRECTAS", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/acevedomateus/MainActivity.java
+++ app/src/main/java/com/example/acevedomateus/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -66,6 +68,15 @@ public class MainActivity extends AppCompatActivity {
         listaPrincipalProductos.add(producto1);
         listaPrincipalProductos.add(producto2);
         listaPrincipalProductos.add(producto3);
+    }
 
+    public void clickCerrarSesion(View view){
+        SharedPreferences miPreferencias = getSharedPreferences("tienda_app", MODE_PRIVATE);
+        SharedPreferences.Editor myEditor = miPreferencias.edit();
+        myEditor.clear();
+        myEditor.apply();
+
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 }
